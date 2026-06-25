0e483119aa26f1e77d2d2b50bf428c0163915940
Debasish Pradhan <dp0000000004@gmail.com>
2026-06-25 08:46:10 +0530

    Add Book model for restaurant reservations

--- restaurants/models.py
+++ restaurants/models.py
@@ -5,3 +5,12 @@ class Restaurant(models.Model):
     description = models.TextField()
     opening_time = models.TimeField()
     closing_time = models.TimeField()
+
+class Book(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    guest_count = models.IntegerField()
+    message = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.guest_count} {self.message}"
